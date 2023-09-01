87507fe6b8fb39c5b2728a9e2527dc778df902f7
Daniil <126776341+daniilshauchenka@users.noreply.github.com>
2023-09-01 16:03:59 +0300

    removed old project files

--- src/main/java/by/htp/ex/util/validation/UserDataValidation.java
+++
@@ -1,9 +0,0 @@
-package by.htp.ex.util.validation;
-
-import java.sql.SQLException;
-
-public interface UserDataValidation {
-       boolean isCorrectPassword(String enteredPassword, String dbPasswordHash) throws SQLException;
-       boolean isCorrectAuthData(String login, String password);
-       
-}
